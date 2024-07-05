0ba17de6b36a4dd5754b66221976ee24ecc3c911
CtrlAltMech <mech@dmux.xyz>
2024-07-05 09:12:25 -0400

    Filled out more variables into the config file. Got very very very basic functionality of looping through directories and pushing files working.

--- btnu.sh
+++ btnu.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
+
 readonly CONFIG="$HOME/git_repos/BackThatNasUp/config"
 
-echo "$CONFIG"
-
+# shellcheck source=./config 
 . "$CONFIG"
 
-
-for i in $DIRECTORIES; do
-    echo "$i"
+# Loops through the list of directories I have in my config file and backs them up.
+# dry-run flag set for testing
+for i in "${DIRECTORIES[@]}" 
+do
+    rsync --dry-run -avzhpe "ssh -i $ONSITE_SSHKEY_PATH" "$i" "$ONSITE_USERNAME"@"$ONSITE_BACKUP_HOST":"$ONSITE_BACKUP_PATH" 
 done
 
-rsync --dry-run -avzhpe ssh /mnt/user/Music mech@thiccpad:/media/veracrypt2/
+#rsync --dry-run -avzhpe ssh /mnt/user/Music mech@thiccpad:/media/veracrypt2/
 
